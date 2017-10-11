f98468fe935f702b6d30f51c5ca20426cf445a30
PtrBld <peter-bludau@gmx.de>
2017-10-11 16:46:03 +0200

    Update MMM-Button.js

--- MMM-Button.js
+++ MMM-Button.js
@@ -10,13 +10,14 @@ Module.register('MMM-Button',{
 	requiresVersion: "2.1.0",
 	defaults: {
 		buttonPIN: 5,
+		notificationMessage: "BUTTON_PRESSED",
 		//time in miliseconds before another button click is recognized
 		clickDelay: 500,		
 	},	
 	// Override socket notification handler.
 	socketNotificationReceived: function(notification, payload) {
-		if (notification === "BUTTON_PRESSED"){
-			this.sendNotification(notification, payload)
+		if (notification === this.config.notificationMessage){
+			this.sendNotification(this.config.notificationMessage, payload)
 		}
 	},	
 	start: function() {
