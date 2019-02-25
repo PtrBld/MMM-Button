c16489cb6f7a821d642b96b343f6f9a94709b73f
PtrBld <peter-bludau@gmx.de>
2019-02-25 09:34:03 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -7,29 +7,27 @@
  */
 
 const NodeHelper = require('node_helper');
-const gpio = require('onoff');
 module.exports = NodeHelper.create({
   start: function () {
-    this.started = false
+    this.started = false;
   },
   // Subclass socketNotificationReceived received.
   socketNotificationReceived: function(notification, payload) {
     const self = this;
     if (notification === 'BUTTON_CONFIG' && this.started == false) {     
-		const self = this
-		this.config = payload     	  
-		var GPIO = require('onoff').Gpio,
-		button = new GPIO(this.config.buttonPIN, 'in', 'both',{ persistentWatch: true, debounceTimeout: this.config.clickDelay });
+		const self = this;
+		this.config = payload;	  
+		var GPIO = require('onoff').Gpio;
+		var button = new GPIO(this.config.buttonPIN, 'in', 'both',{ persistentWatch: true, debounceTimeout: this.config.clickDelay });
 		button.watch(function(err, state) {
-		  // check the state of the button
-		  // 1 == pressed, 0 == not pressed
+		  // check the state of the button; 1 == pressed, 0 == not pressed
 		  if(state == 1) {
 			// send notification for broadcast
 			self.sendSocketNotification(self.config.notificationMessage, true);
 			console.log("button pressed");
 			}
 		});          
-		this.started = true
+		this.started = true;
     };
   }
   
